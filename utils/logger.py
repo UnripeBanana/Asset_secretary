2a70f0e5a9ebd65836457fc72b4dd42a19017763
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 18:08:45 +0900

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,2 +1,7 @@
 from config import NOTION_DB
 from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
+
+def logging():
+  update_time = datetime.now(
+    ZoneInfo("Asia/Seoul")
+  ).strftime("%Y-%m-%d %H:%M")
