cf30f011e4cd0e257c1f954620fde50269613562
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 19:36:13 +0900

    Update main.py

--- domestic_stock/main.py
+++ domestic_stock/main.py
@@ -1,17 +1,15 @@
-from config import NOTION_PRICE_DB_ID
-from notion.get_all_pages import get_all_pages
-
 from domestic_stock.read import get_ticker
 from domestic_stock.update import update_stock_DB
 from data.domestic_stock import get_naver_prop, get_yfinance_prop
 
-for page in get_all_pages(NOTION_PRICE_DB_ID):
-
-  ticker = get_ticker(page)
+def domestic_stock_main(pages)
+  for page in pages:
   
-  stock_info = {
-      **get_naver_prop(ticker),
-      **get_yfinance_prop(ticker)
-  }
-
-  update_stock_DB(stock_info)
+    ticker = get_ticker(page)
+    
+    stock_info = {
+        **get_naver_prop(ticker),
+        **get_yfinance_prop(ticker)
+    }
+  
+    update_stock_DB(stock_info)
