e9be1db10244d07119972bcdd59b63782ea9e77e
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 19:26:53 +0900

    Update main.py

--- domestic_stock/main.py
+++ domestic_stock/main.py
@@ -1,5 +1,7 @@
 from config import NOTION_PRICE_DB_ID
 from notion.get_all_pages import get_all_pages
+from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
+
 
 for page in get_all_pages(DATABASE_ID):
 
