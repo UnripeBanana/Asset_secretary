0311620545333f60878aa42cc3bcff3f6fe8ab9e
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 19:33:58 +0900

    Update main.py

--- domestic_stock/main.py
+++ domestic_stock/main.py
@@ -1,8 +1,9 @@
 from config import NOTION_PRICE_DB_ID
 from notion.get_all_pages import get_all_pages
-from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
+
 from domestic_stock.read import get_ticker
 from domestic_stock.update import update_stock_DB
+from data.domestic_stock import get_naver_prop, get_yfinance_prop
 
 for page in get_all_pages(NOTION_PRICE_DB_ID):
 
