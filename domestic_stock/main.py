2c99a502716bf52ecec713b0de2846fe32ad0eb6
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 19:25:17 +0900

    Update main.py

--- domestic_stock/main.py
+++ domestic_stock/main.py
@@ -1,1 +1,12 @@
 
+
+for page in get_all_pages(DATABASE_ID):
+
+  props = page["properties"]
+
+  ticker_data = props["티커"]["rich_text"]
+
+  if len(ticker_data) == 0:
+      continue
+
+  ticker = ticker_data[0]["plain_text"]
