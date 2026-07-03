0f42fb915cc7c1d210460f68c125c05ab987b931
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 18:04:48 +0900

    Update rebalance.py

--- analysis/rebalance.py
+++ analysis/rebalance.py
@@ -1,3 +1,4 @@
 # 리벨런싱
 
-from config import NOTION_TOKEN 
+from config import NOTION_TOKEN, NOTION_PRICE_DB_ID, NOTION_TRADE_DB_ID
+from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
