03a0dc0cc76ab8357d50dd6bf33c2f2891022d89
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 18:03:31 +0900

    Update read.py

--- notion/read.py
+++ notion/read.py
@@ -1,4 +1,6 @@
-from config import NOTION_TOKEN 
+from config import NOTION_DB
+from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
+
 
 def get_all_pages(database_id):
     pages = []
