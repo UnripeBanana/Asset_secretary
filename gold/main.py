44e5c2f20565a7392d82b79abe4be5066b90938b
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-05 15:14:27 +0900

    Update main.py

--- gold/main.py
+++ gold/main.py
@@ -1,2 +1,9 @@
 from data.domestic_gold import get_gold_price
 
+
+
+"""
+  "price": int(gold["closePrice"].replace(",", "")),           # 현재가
+  "change": int(gold["fluctuations"].replace(",", "")),        # 전일대비
+  "rate": float(gold["fluctuationsRatio"]),                    # 등락률
+"""
