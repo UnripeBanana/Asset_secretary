50adc7ec2bb056eee2eee460191cde5f9932b426
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-05 15:10:26 +0900

    Update domestic_gold.py

--- data/domestic_gold.py
+++ data/domestic_gold.py
@@ -2,11 +2,36 @@
 
 import requests            # 네이버 증권에서 데이터 받아오기
 
-url = "https://m.stock.naver.com/front-api/realTime/marketIndex/metals"
+def get_gold_price():
 
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
+    url = "https://m.stock.naver.com/front-api/realTime/marketIndex/metals"
 
-response = requests.get(url, headers=headers)
-response.raise_for_status()
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    gold = response.json()["result"]["metals"]["M04020000"]
+
+    return {
+        "price": int(gold["closePrice"].replace(",", "")),           # 현재가
+        "change": int(gold["fluctuations"].replace(",", "")),        # 전일대비
+        "rate": float(gold["fluctuationsRatio"]),                    # 등락률
+        "updated": gold["localTradedAt"]                             # 갱신시각
+    }
+
+# 받을 수 있는 정보 모음
+"""
+price = gold["closePrice"]                  # 현재가
+change = gold["fluctuations"]               # 전일대비
+rate = gold["fluctuationsRatio"]            # 등락률
+open_price = gold["openPrice"]              # 시가
+high = gold["highPrice"]                    # 고가
+low = gold["lowPrice"]                      # 저가
+volume = gold["accumulatedTradingVolume"]   # 거래량
+value = gold["accumulatedTradingValue"]     # 거래대금
+status = gold["marketStatus"]               # OPEN / CLOSE
+updated = gold["localTradedAt"]             # 갱신시각
+"""
