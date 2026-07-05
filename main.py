d0ef24b8e9cd40b0b39c35c3f55be196a61d3d05
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-05 12:54:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from config import NOTION_PRICE_DB_ID, NOTION_TRADE_DB_ID
 from notion.get_all_pages import get_all_pages
 
 from domestic_stock.main import domestic_stock_main
-from domestic_stock_trade.main import domestic_stock_main
+from domestic_stock_trade.main import domestic_stock_trade_main
 
 
 domestic_stock_main(get_all_pages(NOTION_PRICE_DB_ID))
