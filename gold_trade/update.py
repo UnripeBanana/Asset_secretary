a857fac4f4d12138b3d1ef32374a5a986a9b4d30
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-09 12:43:08 +0900

    Update update.py

--- gold_trade/update.py
+++ gold_trade/update.py
@@ -1,1 +1,20 @@
+from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
 
+def update_trade_page(page_id, remaining=None, profit=None):
+
+    properties = {}
+
+    if remaining is not None:
+        properties["잔량"] = {
+            "number": remaining
+        }
+
+    if profit is not None:
+        properties["실현수익"] = {
+            "number": profit
+        }
+
+    notion.pages.update(
+        page_id=page_id,
+        properties=properties
+    )
