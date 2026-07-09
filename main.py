63efdd66c1b906296c3e01760aae38326b257bc8
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-09 20:39:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from config import NOTION_PRICE_DB_ID, NOTION_TRADE_DB_ID, NOTION_KRX_GOLD_INFO_DB_ID, NOTION_KRX_GOLD_TRADE_DB_ID
+from config import NOTION_PRICE_DB_ID, NOTION_TRADE_DB_ID, NOTION_KRX_GOLD_INFO_DB_ID, NOTION_KRX_GOLD_TRADE_DB_ID, NOTION_DOMESTIC_BOND_ETF_DB_ID
 from notion.get_all_pages import get_all_pages
 
 from domestic_stock.main import domestic_stock_main
@@ -13,7 +13,7 @@ from gold_trade.main import gold_trade_main
 
 # 개발 진행 중
 #gold_trade_main(NOTION_KRX_GOLD_TRADE_DB_ID)
-domestic_bond_etf_DB_main()
+domestic_bond_etf_DB_main(get_all_pages(NOTION_DOMESTIC_BOND_ETF_DB_ID))
 
 """
 # 프로젝트의 시작점.
