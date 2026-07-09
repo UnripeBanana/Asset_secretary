80fecc1a98463f453d8d0dd2bda20285761273ec
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-09 19:00:10 +0900

    Update main.py

--- domestic_stock/main.py
+++ domestic_stock/main.py
@@ -1,18 +1,34 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from domestic_stock.read import get_ticker
 from domestic_stock.update import update_stock_DB
 from data.domestic_stock import get_naver_prop, get_yfinance_prop
+from history.history import append_prices
 
-def domestic_stock_main (pages):
-  for page in pages:
-  
-    ticker = get_ticker(page)
+def domestic_stock_main(pages):
+    today = datetime.now(ZoneInfo("Asia/Seoul")).strftime("%Y-%m-%d")
+    history_rows = []
 
-    if not ticker:
-      continue
-    
-    stock_info = {
-        **get_naver_prop(ticker),
-        **get_yfinance_prop(ticker)
-    }
-  
-    update_stock_DB(page, stock_info)
+    for page in pages:
+        ticker = get_ticker(page)
+        if not ticker:
+            continue
+
+        stock_info = {
+            **get_naver_prop(ticker),
+            **get_yfinance_prop(ticker)
+        }
+
+        update_stock_DB(page, stock_info)
+
+        history_rows.append({
+            "date": today,
+            "ticker": ticker,
+            "open": stock_info["open"],
+            "high": stock_info["high"],
+            "low": stock_info["low"],
+            "close": stock_info["price"],
+            "volume": stock_info["volume"]
+        })
+
+    append_prices(history_rows)
